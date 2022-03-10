722741d9ad781aa8a8141536d9ff0e767a613efc
UH5555 <zhuhao162@163.com>
2022-03-10 21:46:49 +0800

    0310\作业-递归

--- test.c
+++ test.c
@@ -215,3 +215,108 @@
 //	print_table(n);
 //	return 0;
 //}
+
+////字符串逆序
+//#include <string.h>
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char arr[])
+//{
+//	int left = 0;
+//	int right = my_strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//void reverse_string(char arr[])
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if(my_strlen(arr+1)>=2)
+//		reverse_string(arr + 1);
+//	arr[len - 1] = tmp;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+////返回每位数之和
+//int Digitsum(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		return (Digitsum(num / 10) + (num % 10));
+//	}
+//	else
+//		return num;
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	int ret = Digitsum(num);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+////n的k次方
+//int Pow(int n, int k)
+//{
+//	if (k < 0)
+//		return (1.0 / Pow(n, -k));
+//	else if (k == 0)
+//		return 1;
+//	else
+//		return n * Pow(n, k - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n, &k);
+//	double ret = Pow(n, k);
+//	printf("ret=%lf\n", ret);
+//	return 0;
+//}
